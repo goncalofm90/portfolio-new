df80593b7bb99136f6ff2e24f3eb95c05a29743d
goncalom90 <136586500+goncalom90@users.noreply.github.com>
2025-08-11 19:32:03 +0100

    use section component

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,32 @@ import Experience from "./Components/Experience";
 import Projects from "./Components/Projects";
 import TechStack from "./Components/TechStack";
 import ScrollSnapWrapper from "./Components/ui/scroll-snap-wrapper";
-
+import Section from "./Components/Section"; // your reusable section component
 
 function App() {
   return (
     <ScrollSnapWrapper>
-      <About />
-      <TechStack />
-      <Projects />
-      <Experience />
-      <Contact />
+      
+      <Section snap>
+        <About />
+      </Section>
+
+      <Section snap>
+        <TechStack />
+      </Section>
+
+      <Section snap>
+        <Projects />
+      </Section>
+
+      {/* snap has to be false */}
+      <Section>
+        <Experience />
+      </Section>
+
+      <Section snap>
+        <Contact />
+      </Section>
     </ScrollSnapWrapper>
   );
 }
