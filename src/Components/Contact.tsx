2514d2215f18b3bb7a9a1c26d4c3e96e52a58a0b
goncalofm90 <goncalofm90@gmail.com>
2025-08-17 17:03:03 +0100

    contact page icons and components

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,19 +1,38 @@
+import React from "react";
+import { Vortex } from "../Components/ui/vortex";
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
+import { TbFileCv } from "react-icons/tb";
+import GradientIconButton from "./ui/button";
 
-function Contact() {
+const Contact: React.FC = () => {
   return (
-    <section className="bg-black min-h-screen max-h-screen w-full overflow-hidden snap-child h-screen">
-      <div className="lg:flex w-full justify-between max-w-full">
-       {/* Left side text */}
-        <div className="flex-1 py-24 lg:py-48 lg:px-8">
-          <div>
-            <div className="mt-10 flex-col items-center gap-x-6">
-              <h1 className="text-white text-center">Contact</h1>
-            </div>
-          </div>
+    <div className="w-[calc(100%-4rem)] mx-auto rounded-md h-full overflow-hidden">
+      <Vortex
+        backgroundColor="black"
+        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+      >
+        <h2 className="text-white text-8xl md:text-8xl text-center text-outline">
+          Get in touch
+        </h2>
+        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
+          <GradientIconButton
+            icon={CiLinkedin}
+            url="https://www.linkedin.com/in/goncalo-mendes/"
+          />
+          <GradientIconButton
+            icon={FaGithub}
+            url="https://github.com/goncalofm90"
+          />
+          <GradientIconButton
+            icon={TbFileCv}
+            url="/CVGM2025.pdf"
+            download="CVGM2025.pdf"
+          />
         </div>
-      </div>
-    </section>
+      </Vortex>
+    </div>
   );
-}
+};
 
 export default Contact;
---
+++ src/Components/ui/button.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+interface GradientIconButtonProps {
+  icon: React.ElementType;
+  url?: string;
+  onClick?: () => void;
+  download?: string;
+}
+
+const GradientIconButton: React.FC<GradientIconButtonProps> = ({
+  icon: Icon,
+  url,
+  onClick,
+  download,
+}) => {
+  const Component = url ? "a" : "button";
+
+  return (
+    <Component
+      href={url}
+      onClick={onClick}
+      target={url && !download ? "_blank" : undefined}
+      rel={url && !download ? "noopener noreferrer" : undefined}
+      download={download}
+      className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden 
+                 text-base font-medium text-gray-900 rounded-lg group 
+                 bg-gradient-to-br from-purple-600 to-blue-500 
+                 group-hover:from-purple-600 group-hover:to-blue-500 
+                 hover:text-white dark:text-white 
+                 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 outline-double"
+    >
+      <span
+        className="relative px-4 py-1 transition-all ease-in duration-75 
+                   bg-white dark:bg-black rounded-md 
+                   group-hover:bg-transparent group-hover:dark:bg-transparent"
+      >
+        <Icon size={22} />
+      </span>
+    </Component>
+  );
+};
+
+export default GradientIconButton;
