03a4d209b4fd6e67450e17ddc9177859398da897
goncalofm90 <goncalofm90@gmail.com>
2025-08-17 19:03:49 +0100

    enlarge on hover

--- src/Components/ui/button.tsx
+++ src/Components/ui/button.tsx
@@ -27,7 +27,9 @@ const GradientIconButton: React.FC<GradientIconButtonProps> = ({
                  bg-gradient-to-br from-purple-600 to-blue-500 
                  group-hover:from-purple-600 group-hover:to-blue-500 
                  hover:text-white dark:text-white 
-                 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 outline-double"
+                 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 
+                 outline-double 
+                 transform transition-all duration-200 hover:scale-110"
     >
       <span
         className="relative px-4 py-1 transition-all ease-in duration-75 
